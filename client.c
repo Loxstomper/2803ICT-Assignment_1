a34bae12a16b82b76a9eb24875f98b56233f2ce2
loxstomper <lochieashcroft@gmail.com>
2018-08-22 15:37:19 +1000

    Single client

--- client.c
+++ client.c
@@ -4,5 +4,71 @@
 
 int main(int argc, char ** argv)
 {
+    
+    /* if (argc != 2) */
+    /* { */
+    /*     printf("./client IP\n"); */
+    /*     return 1; */
+    /* } */
+
+
+    char message[1024];
+    char reply[2048];
+    int sock;
+
+    struct sockaddr_in server;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock == -1)
+    {
+        printf("Could not create socket\n");
+        return -1;
+    }
+
+    printf("Created socket\n");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* server.sin_addr.s_addr = inet_addr(argv[1]); */
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0)
+    {
+        printf("Connection Failed!\n");
+        return -1;
+    }
+
+    printf("Connected\n");
+
+    while (1)
+    {
+        printf("\nRequest: ");
+        fgets(message, 1024, stdin);
+
+        if (send(sock, message, strlen(message), 0) < 0)
+        {
+            printf("Send Failed!\n");
+            return -1;
+        }
+
+        if ((strcmp(message, "quit\n")) == 0)
+        {
+            break;
+        }
+
+        if (recv(sock, reply, 2048, 0) < 0)
+        {
+            printf("Recv Failed!\n");
+            return -1;
+        }
+
+        printf("Reply: %s\n", reply);
+        memset(&reply, '\0', strlen(reply));
+        memset(&message, '\0', strlen(message));
+    }
+
+    close(sock);
+
     return 0;
 }
--- common.h
+++ common.h
@@ -1,4 +1,13 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#define PORT 80
+
 #endif
